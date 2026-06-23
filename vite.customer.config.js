c1d0c311a6f3876f1df58de3a7499a1037446b6c
Jerry <jerry@Jerrys-MacBook-Pro.local>
2026-06-23 14:08:37 +0700

    Fix builds: rename HTML files to index.html for Cloudflare Pages

--- vite.customer.config.js
+++ vite.customer.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    rollupOptions: {
+      input: "index-customer.html",
+    },
     outDir: "dist-customer",
     emptyOutDir: true,
   },
